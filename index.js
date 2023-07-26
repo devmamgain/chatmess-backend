b768ab377260e3eff77c35715be1c290654e9e11
Dev Mamgain <112395945+devmamgain@users.noreply.github.com>
2023-07-26 12:02:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const server=app.listen(process.env.PORT,()=>{
 
 const io =new Server(server,{
     cors:{
-        origin: "http://localhost:3000",
+        origin: "https://chatmess-frontend.vercel.app",
     },
 })
 global.onlineUsers = new Map()
